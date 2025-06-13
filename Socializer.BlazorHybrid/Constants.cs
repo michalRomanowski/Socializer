a4bb21c4e05dcb7a2cebcf8308780fc4be2ef5be
DESKTOP-N5D7U6V\micha <michal.romanowski87@gmail.com>
2025-06-13 11:53:07 +0200

    Cosmethic, some redundand comments removal

--- Socializer.BlazorHybrid/Constants.cs
+++ Socializer.BlazorHybrid/Constants.cs
@@ -4,7 +4,7 @@ internal class Constants
 {
     /// <summary>
     /// Config server url must be hardcoded one way or another
-    /// TODO: Some extra security should be applied, like additional encoding ?
+    /// TODO: Some extra security should be applied, like additional encoding
     /// </summary>
     public const string ConfigUrl = "https://d20c-79-144-138-103.ngrok-free.app" + "/Config";
 }
