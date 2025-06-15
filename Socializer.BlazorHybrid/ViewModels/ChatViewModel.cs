8292ea68d780d145cd3e1e073ebe1724da5e7815
DESKTOP-N5D7U6V\micha <michal.romanowski87@gmail.com>
2025-06-15 15:58:01 +0200

    Add chat FE

--- Socializer.BlazorHybrid/ViewModels/ChatViewModel.cs
+++ Socializer.BlazorHybrid/ViewModels/ChatViewModel.cs
@@ -1,19 +1,25 @@
-﻿
-using Syncfusion.Blazor.InteractiveChat;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Socializer.BlazorHybrid.ViewModels
+﻿namespace Socializer.BlazorHybrid.ViewModels
 {
+    internal class ChatMessage
+    {
+        public string Author { get; set; }
+        public string Content { get; set; }
+    }
+
     internal class ChatViewModel
     {
-        
-        public List<ChatMessage> Messages { get; set; } = [new ChatMessage() { Text = "DUPA" }];
+        public string? NewMessage { get; set; }
 
-
+        public List<ChatMessage> Messages { get; set; } = [ 
+            new ChatMessage(){ Author = "user1", Content = "Message1"},
+            new ChatMessage(){ Author = "user2", Content = "Message2"},
+            new ChatMessage(){ Author = "user2", Content = "Message3"},
+            new ChatMessage(){ Author = "user3", Content = "Message4"},
+            new ChatMessage(){ Author = "user1", Content = "Message5"},
+            new ChatMessage(){ Author = "user1", Content = "Message6"},
+            new ChatMessage(){ Author = "user1", Content = "Message7"},
+            new ChatMessage(){ Author = "user2", Content = "Message8"},
+            new ChatMessage(){ Author = "user3", Content = "Message9"},
+        ];
     }
 }
