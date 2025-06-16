e3ed995b1dd32d5724289e7d886c252e76820088
DESKTOP-N5D7U6V\micha <michal.romanowski87@gmail.com>
2025-06-16 18:36:14 +0200

    Message color according to user type user, other user, bot

--- Socializer.BlazorHybrid/ViewModels/ChatViewModel.cs
+++ Socializer.BlazorHybrid/ViewModels/ChatViewModel.cs
@@ -10,14 +10,14 @@
     {
         public string NewMessage { get; set; } = string.Empty;
 
-        public List<ChatMessage> Messages { get; set; } = [ 
-            new ChatMessage(){ Author = "user1", Content = "Message1"},
+        public List<ChatMessage> Messages { get; set; } = [
+            new ChatMessage(){ Author = "Bot", Content = "Message1"},
             new ChatMessage(){ Author = "user2", Content = "Message2"},
-            new ChatMessage(){ Author = "user2", Content = "Message3"},
+            new ChatMessage(){ Author = "Michal", Content = "Message3"},
             new ChatMessage(){ Author = "user3", Content = "Message4"},
-            new ChatMessage(){ Author = "user1", Content = "Message5"},
+            new ChatMessage(){ Author = "Bot", Content = "Message5"},
             new ChatMessage(){ Author = "user1", Content = "Message6"},
-            new ChatMessage(){ Author = "user1", Content = "Message7"},
+            new ChatMessage(){ Author = "Michal", Content = "Message7"},
             new ChatMessage(){ Author = "user2", Content = "Message8"},
             new ChatMessage(){ Author = "user3", Content = "Message9"},
         ];
