ee75cd2958dbb1a45316ffd37f06de906519876a
DESKTOP-N5D7U6V\micha <michal.romanowski87@gmail.com>
2025-06-24 11:50:38 +0200

    Add keyboard display config for Android

--- Socializer.BlazorHybrid/App.xaml.cs
+++ Socializer.BlazorHybrid/App.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace Socializer.BlazorHybrid
+﻿using Microsoft.Maui.Controls.PlatformConfiguration.AndroidSpecific;
+
+namespace Socializer.BlazorHybrid
 {
-    public partial class App : Application
+    public partial class App : Microsoft.Maui.Controls.Application
     {
         public App()
         {
             InitializeComponent();
+
+            // For keyboard behavior not overshadowing the input field in Android
+            On<Microsoft.Maui.Controls.PlatformConfiguration.Android>()
+                .UseWindowSoftInputModeAdjust(WindowSoftInputModeAdjust.Resize);
 
             MainPage = new MainPage();
         }
