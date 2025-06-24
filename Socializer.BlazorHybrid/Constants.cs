41206686c4257693f5490b12a0f79e441f4cb9be
DESKTOP-N5D7U6V\micha <michal.romanowski87@gmail.com>
2025-06-24 11:50:11 +0200

    Tweaks for scroll

--- Socializer.BlazorHybrid/Constants.cs
+++ Socializer.BlazorHybrid/Constants.cs
@@ -6,5 +6,5 @@ internal class Constants
     /// Config server url must be hardcoded one way or another
     /// TODO: Some extra security should be applied, like additional encoding
     /// </summary>
-    public const string ConfigUrl = "https://09ab-5-172-255-197.ngrok-free.app" + "/Config";
+    public const string ConfigUrl = "https://5fa3-31-0-125-21.ngrok-free.app" + "/Config";
 }
--- Socializer.BlazorHybrid/wwwroot/chat.js
+++ Socializer.BlazorHybrid/wwwroot/chat.js
@@ -3,3 +3,8 @@
         element.scrollTop = element.scrollHeight;
     }
 };
+
+window.isScrollAtBottom = (element) => {
+    // Check if scrolled to bottom (within a small threshold)
+    return (element.scrollHeight - element.scrollTop - element.clientHeight) < 5;
+};
