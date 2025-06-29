366c5ac2fee3d6975324f59f2fc44b53def43032
DESKTOP-N5D7U6V\micha <michal.romanowski87@gmail.com>
2025-06-29 14:48:06 +0200

    Message formatting in chat for endlines

---
+++ Socializer.BlazorHybrid/Extensions/MarkupStringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Socializer.BlazorHybrid.Extensions
+{
+    internal static class MarkupStringExtensions
+    {
+        public static MarkupString ToHtmlBreaks(this string str)
+        {
+            return new MarkupString(str.Replace("\n", "<br>").Replace("\r", string.Empty));
+        }
+    }
+}
