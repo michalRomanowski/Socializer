27b1bed772ddf2e83620ab6965a7131e01ecd01a
DESKTOP-N5D7U6V\micha <michal.romanowski87@gmail.com>
2025-06-29 16:14:20 +0200

    Add more preferences degugging messages

--- Socializer.API/SignalR/ChatHub.cs
+++ Socializer.API/SignalR/ChatHub.cs
@@ -4,15 +4,15 @@ using Socializer.LLM;
 
 namespace Socializer.API.SignalR;
 
-public class ChatHub(ILLMClient lLMClient, IPreferenceService preferenceService, ILogger<ChatHub> logger) : Hub
+public class ChatHub(ILLMClient lLMClient, IPreferenceService preferenceService, IUserService userService, ILogger<ChatHub> logger) : Hub
 {
-    public async Task SendMessage(string user, string message)
+    public async Task SendMessage(string username, string message)
     {
         try
         {
-            logger.LogDebug("Received message: '{Message}' from User: '{User}'.", message, user);
+            logger.LogDebug("Received message: '{Message}' from User: '{User}'.", message, username);
 
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", username, message);
 
             var llmResponse = await lLMClient.QueryAsync(message, 200); // TODO: Limit can be configurable
             await Clients.All.SendAsync("ReceiveMessage", "bot", llmResponse);
@@ -20,10 +20,14 @@ public class ChatHub(ILLMClient lLMClient, IPreferenceService preferenceService,
             await Task.Delay(10000); // TODO: Delay added because of requests limit for free models
 
             var preferences = await preferenceService.GetPreferencesAsync(message);
-            var preferencesMessage = string.Join("\r\n", preferences.Select(x => $"{x.PreferenceType} {x.Url}"));
+            var updatedUser = await userService.AddPreferencesAsync(username, preferences);
+
+            var newPreferencesMessage = string.Join("\r\n", preferences.Select(x => $"{x.PreferenceType} {x.Url}"));
+            var allPreferencesMessage = string.Join("\r\n", updatedUser.Preferences.Select(x => $"{x.PreferenceType} {x.Url}"));
 
             // TODO: for debug purposes, add config feature flag
-            await Clients.All.SendAsync("ReceiveMessage", "bot", preferencesMessage);
+            await Clients.All.SendAsync("ReceiveMessage", "bot", newPreferencesMessage);
+            await Clients.All.SendAsync("ReceiveMessage", "bot", allPreferencesMessage);
         }
         catch (Exception ex){
             logger.LogError(ex, "Exception in chat.");
