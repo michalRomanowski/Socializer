36f04730d9682ea503456797d06b552c1da35729
DESKTOP-N5D7U6V\micha <michal.romanowski87@gmail.com>
2025-06-29 16:15:17 +0200

    Migration for preference type

---
+++ Socializer.Database/Migrations/SocializerDb/20250629130641_AddPreferenceType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Socializer.Database.Migrations.SocializerDb
+{
+    /// <inheritdoc />
+    public partial class AddPreferenceType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PreferenceType",
+                table: "Preferences",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PreferenceType",
+                table: "Preferences");
+        }
+    }
+}
