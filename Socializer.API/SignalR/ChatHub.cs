16eff1d2f403f378890f292dc0ed5d3857e0066f
DESKTOP-N5D7U6V\micha <michal.romanowski87@gmail.com>
2025-06-29 14:46:44 +0200

    Error handling and tokens limitation for chat

--- Socializer.API/SignalR/ChatHub.cs
+++ Socializer.API/SignalR/ChatHub.cs
@@ -8,19 +8,25 @@ public class ChatHub(ILLMClient lLMClient, IPreferenceService preferenceService,
 {
     public async Task SendMessage(string user, string message)
     {
-        logger.LogDebug("Received message: '{Message}' from User: '{User}'.", message, user);
+        try
+        {
+            logger.LogDebug("Received message: '{Message}' from User: '{User}'.", message, user);
 
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
 
-        var llmResponse = await lLMClient.QueryAsync(message);
+            var llmResponse = await lLMClient.QueryAsync(message, 200); // TODO: Limit can be configurable
+            await Clients.All.SendAsync("ReceiveMessage", "bot", llmResponse);
 
-        await Task.Delay(10000); // TODO: Delay added because of requests limit for free models
+            await Task.Delay(10000); // TODO: Delay added because of requests limit for free models
 
-        var preferences = await preferenceService.GetPreferencesAsync(message);
+            var preferences = await preferenceService.GetPreferencesAsync(message);
+            var preferencesMessage = string.Join("\r\n", preferences.Select(x => $"{x.PreferenceType} {x.Url}"));
 
-        await Clients.All.SendAsync("ReceiveMessage", "bot", llmResponse);
-
-        // TODO: for debug purposes, add config feature flag
-        await Clients.All.SendAsync("ReceiveMessage", "bot", preferences.Select(x => $"{x.PreferenceType} {x.Url}"));
+            // TODO: for debug purposes, add config feature flag
+            await Clients.All.SendAsync("ReceiveMessage", "bot", preferencesMessage);
+        }
+        catch (Exception ex){
+            logger.LogError(ex, "Exception in chat.");
+        }
     }
 }
