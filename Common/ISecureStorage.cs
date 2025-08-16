581ca23b80fd0d53327ffd1dd3aaee35d486e798
DESKTOP-N5D7U6V\micha <michal.romanowski87@gmail.com>
2025-08-16 18:43:20 +0200

    Add interface over secure storage

---
+++ Common/ISecureStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Utils;
+
+public interface ISecureStorage
+{
+    Task<string?> GetAsync(string key);
+    Task SetAsync(string key, string value);
+    bool Remove(string key);
+    void RemoveAll();
+}
