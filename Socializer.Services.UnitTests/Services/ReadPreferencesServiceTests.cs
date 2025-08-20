20ef378db2d99bdb7b4a27653fe745cd32d3ec57
DESKTOP-N5D7U6V\micha <michal.romanowski87@gmail.com>
2025-08-20 14:22:45 +0200

    Unit test for read preferences

---
+++ Socializer.Services.UnitTests/Services/ReadPreferencesServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Logging.Abstractions;
+using Socializer.Database.Models;
+using Socializer.Services.Services;
+
+namespace Socializer.Services.UnitTests.Services
+{
+    public class ReadPreferencesServiceTests
+    {
+        private readonly ReadPreferencesService sut;
+
+        public ReadPreferencesServiceTests()
+        {
+            sut = new ReadPreferencesService(NullLogger<ReadPreferencesService>.Instance);
+        }
+
+        [Theory]
+        [MemberData(nameof(PreferencesData))]
+        public void ReadPreferences_ReturnsExpectedPreferences(string preferencesFromLLM, IEnumerable<Preference> expectedPreferences)
+        {
+            // Act
+            var result = sut.ReadPreferences(preferencesFromLLM);
+
+            // Assert
+            expectedPreferences.Select(x => x.DBPediaResource).Should().BeEquivalentTo(result.Select(r => r.DBPediaResource));
+        }
+
+        public static IEnumerable<object[]> PreferencesData =>
+            [
+                [
+                    "resource1", // string parameter
+                    new Preference[] { new() { DBPediaResource = "resource1" } } // array parameter
+                ]
+            ];
+    }
+}
