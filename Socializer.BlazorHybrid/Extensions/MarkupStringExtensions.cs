2d8d4d586c7c2c8bb8ae77a2c6badd464ddd187f
DESKTOP-N5D7U6V\micha <michal.romanowski87@gmail.com>
2025-09-24 13:03:59 +0200

    Guard against empty input

--- Socializer.BlazorHybrid/Extensions/MarkupStringExtensions.cs
+++ Socializer.BlazorHybrid/Extensions/MarkupStringExtensions.cs
@@ -4,8 +4,6 @@ namespace Socializer.BlazorHybrid.Extensions;
 
 internal static class MarkupStringExtensions
 {
-    public static MarkupString ToHtmlBreaks(this string str)
-    {
-        return new MarkupString(str.Replace("\n", "<br>").Replace("\r", string.Empty));
-    }
+    public static MarkupString ToHtmlBreaks(this string str) =>
+        new(string.IsNullOrEmpty(str) ? string.Empty : str.Replace("\n", "<br>").Replace("\r", string.Empty));
 }
